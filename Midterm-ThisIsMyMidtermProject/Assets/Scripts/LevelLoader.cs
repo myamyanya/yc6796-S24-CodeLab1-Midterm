71589a5c71da6a72e8bccabaee7be3adc7b48fa0
VivianC <37090776+myamyanya@users.noreply.github.com>
2024-03-09 02:52:24 -0500

    Add Json lib

--- Midterm-ThisIsMyMidtermProject/Assets/Scripts/LevelLoader.cs
+++ Midterm-ThisIsMyMidtermProject/Assets/Scripts/LevelLoader.cs
@@ -1,14 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class LevelLoader : MonoBehaviour
 {
+    public static LevelLoader instance;
     
+    // File path
+    private string FIRE_PATH;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // For reading all text from the .txt file
+    private string fileContent;
     
     // Start is called before the first frame update
     void Start()
     {
+        FIRE_PATH = Application.dataPath + "/Levels/Levels.txt";
+        if (File.Exists(FIRE_PATH))
+        {
+            fileContent = File.ReadAllText(FIRE_PATH);
+        }
+        
         LoadLevel();
     }
 
